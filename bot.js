51ac2d7e8c334876d7675999b40921f82a78a49f
Zaàrex <45122766+JuustZayd5K@users.noreply.github.com>
2019-01-02 22:07:08 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const developers = ['343743154429755392','527028584787869696'];
+
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!developers.includes(message.author.id)) return;
+
+if (message.content.startsWith('Hwt')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+  client.user.setStatus("dnd")
+    message.channel.send(` ** Done. :white_check_mark: ** `)
+} else 
+if (message.content.startsWith('Hls')) {
+client.user.setActivity(argresult , {type:'LISTENING'});
+  client.user.setStatus("idle")
+    message.channel.send(` ** Done. :white_check_mark: ** `)
+} else 
+if (message.content.startsWith('Hst')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/iitxbro");
+  client.user.setStatus("dnd")
+   message.channel.send(` ** Done. :white_check_mark: ** `)
+} else 
+if (message.content.startsWith('Hpl')) {
+client.user.setActivity(argresult , {type:'PLAYING'});
+  client.user.setStatus("idle")
+    message.channel.send(` ** Done. :white_check_mark: ** `)
+
+}
+ 
